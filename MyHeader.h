4b0334be04e593c52eacbf6b05f856c5d309c697
poojagupta18 <git config --global user.email>
2019-07-25 20:47:05 +0530

    Final Commit

--- MyHeader.h
+++ MyHeader.h
@@ -1,19 +1,23 @@
+//
+//  @Author: Pooja Gupta
+//  @Date: 25/7/19
+//	@Purpose: Provide Process Explorer Functionality
+//
+
 #define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
 #include<windows.h>
 #include<TlHelp32.h>
-#include <tchar.h>
+#include<tchar.h>
 #include<conio.h>
 #include<string>
 #include<io.h>
 #include<stdio.h>
-#include<stdlib.h>
 
 using namespace std;
 
-//typedef for a structure for writing process information into a log file
 
-typedef struct LogFile
+typedef struct LogFile   //typedef for a structure of logFile for writing process information into a log file
 {
 	char ProcessName[100];
 	unsigned int pid;
